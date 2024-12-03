e931be380b1f2847981a7aa5e3045a96ed90cd04
Pranav Verma <142308925+pranav89624@users.noreply.github.com>
2024-12-03 21:12:40 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import summarizeText from './summarize.js';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use(express.static('../public'));
+
+app.post('/summarize', (req, res) => {
+
+  const text = req.body.text_to_summarize;
+
+  summarizeText(text)
+    .then(response => {
+      res.send(response);
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
---
+++ backend/summarize.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import 'dotenv/config';
+
+async function summarizeText(text) {
+
+  let data = JSON.stringify({
+    "inputs": text,
+    "parameters": {
+      "max_length": 100,
+      "min_length": 30
+    }
+  });
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + process.env.ACCESS_TOKEN
+    },
+    data: data
+  };
+
+
+  try {
+    const response = await axios.request(config);
+    return response.data[0].summary_text;
+  }
+  catch (error) {
+    console.log(error);
+  }
+
+}
+
+export default summarizeText;
